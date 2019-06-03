86d7d06a3c385d1b893b22b01c5ed7a2c88b32a9
Arun Kumar <antony@123789.org>
2019-06-03 16:20:59 +0530

    updated

---
+++ TestUtils/TestUtils.h
@@ -0,0 +1,19 @@
+//
+//  TestUtils.h
+//  TestUtils
+//
+//  Created by Admin on 6/3/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestUtils.
+FOUNDATION_EXPORT double TestUtilsVersionNumber;
+
+//! Project version string for TestUtils.
+FOUNDATION_EXPORT const unsigned char TestUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestUtils/PublicHeader.h>
+
+
